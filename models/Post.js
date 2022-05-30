74ee97af0a54de7ca51276a5c80911e39ee6bf6d
Robert Pliszka <robplisz@gmail.com>
2022-05-30 16:50:03 -0400

    Created Post model in Post.js

--- models/Post.js
+++ models/Post.js
@@ -4,5 +4,39 @@ const bcrypt = require('bcrypt');
 
 class Post extends Model {}
 
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        post_url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isURL: true
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequalize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
 
 module.exports = Post;
