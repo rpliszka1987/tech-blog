d9722b90f2ad0359a7579795a7dfe6f8a8c44438
Robert Pliszka <robplisz@gmail.com>
2022-06-05 19:11:21 -0400

    Finished styling the views.

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -19,7 +19,7 @@ router.post('/', (req, res) => {
             {
                 comment_text: req.body.comment_text,
                 post_id: req.body.post_id,
-                // use the id fomr the session
+                // use the id from the session
                 user_id: req.session.user_id
             }
         )
